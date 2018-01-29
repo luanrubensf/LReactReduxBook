6fabe9df61453613b66b8d09bd81aae0abb9ad5d
luanfigueredo <luanrubensf@gmail.com>
2018-01-29 00:04:53 -0200

    Books project commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# New project
+
+Sample project created in order to learn more about React and Redux.
+
+It's a simple book list that uses Redux concepts.
+
+![alt text](https://github.com/luanrubensf/LReactReduxBook/blob/master/doc/app-print.png)
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+    return [
+        { title: 'Learning Python', pages: 345},
+        { title: 'Mastering Ubuntu Server', pages: 300 },
+        { title: 'Smart Internet of Things Projects', pages: 267 },
+        { title: 'Mastering Blockchain', pages: 598 },
+        { title: 'Go Programming Blueprints', pages: 245 }
+    ];
+}
